5f283e0549c611a9a966c26f4edc96d84c0f5921
Rishaad Khan <71447325+maniac-pshyco@users.noreply.github.com>
2023-04-24 10:05:49 +0530

    Update Home.js

--- src/core/Home.js
+++ src/core/Home.js
@@ -24,9 +24,9 @@ export default function Home() {
   }, []);
 
   return (
-    <Base title="Home Page" description="Welcome to the Tshirt Store">
+    <Base title="Home Page" description="Welcome to the Store">
       <div className="row text-center">
-        <h1 className="text-white">All of tshirts</h1>
+        <h1 className="text-white">All of our Products</h1>
         <div className="row">
           {products.map((product, index) => {
             return (
